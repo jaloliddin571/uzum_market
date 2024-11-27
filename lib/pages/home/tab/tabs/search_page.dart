87c22c06dfb4241014c6320c28337780676fa0d7
jaloliddin571 <muhhammadjaloliddin@.com>
2024-11-27 21:19:53 +0300

    splash page done

--- lib/pages/home/tab/tabs/search_page.dart
+++ lib/pages/home/tab/tabs/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uzum_market/components/constants/image.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:uzum_market/components/constants/image.dart';
+import 'package:uzum_market/pages/home/tab/tabs/main_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Future.delayed(
+      Duration(seconds: 3),
+    ).then((value) =>
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (_) => MainPage()),
+              (route) => false,
+        ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          AppImages.logo,
+          width: 120,
+        ),
+      ),
+    );
   }
 }
