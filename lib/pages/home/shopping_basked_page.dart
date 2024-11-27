7b8f30aa1e1b11b269ecd2b0cc0a52b0eeb84c4f
jaloliddin571 <muhhammadjaloliddin@.com>
2024-11-27 20:41:31 +0300

    project files created

---
+++ lib/pages/home/shopping_basked_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingBaskedPage extends StatefulWidget {
+  const ShoppingBaskedPage({super.key});
+
+  @override
+  State<ShoppingBaskedPage> createState() => _ShoppingBaskedPageState();
+}
+
+class _ShoppingBaskedPageState extends State<ShoppingBaskedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
